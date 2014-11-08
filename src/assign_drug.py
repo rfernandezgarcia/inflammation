84f154bc165b47fc13f91e8cb7765908ca6e26df
Rosa Fernandez <rfernandezgarcia@g.harvard.edu>
2014-11-08 15:02:06 -0500

    Script to assign drug to a file

---
+++ src/assign_drug.py
@@ -0,0 +1,14 @@
+def assign_drug(filename):
+	number = filename[13:-4]
+	result = ''
+	if (int(number)%2) == 1:
+		result = 'tylenol'
+	else:
+		result = 'placebo'
+	return result
+
+import sys
+filename = sys.argv[1]
+print assign_drug(filename)
+assert assign_drug('inflammation_1.dat') == 'tylenol'
+assert assign_drug('inflammation_4.dat') == 'placebo'
